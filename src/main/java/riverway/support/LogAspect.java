819113afafca515c07cc6f4c842fa467844c24d8
KangGilHwan <rlfghksop@naver.com>
2019-01-29 03:01:40 +0900

    Modify LogAspect code

--- src/main/java/riverway/support/LogAspect.java
+++ src/main/java/riverway/support/LogAspect.java
@@ -1,7 +1,6 @@
 package riverway.support;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
